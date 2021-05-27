e147e6abb7aa749cdc4dd6621bdd88cb9307b7f3
FarisPalayi <fariswritecode@gmail.com>
2021-05-27 20:55:29 +0530

    added dislike button

--- instance/instance.js
+++ instance/instance.js
@@ -3,6 +3,12 @@
 let vm = new Vue({
     el: '#app',
     data: {
-        likes: 0
+        likes: 0,
+        color: 'color: currentColor'
+    },
+    methods: {
+        giveColor: function() {
+            if (this.likes > 0) this.color = 'color:#1cff00'
+        }
     }
 })
