4aa7dcfa3eda924dbcaa876861cffb9d0f0086d8
FarisPalayi <fariswritecode@gmail.com>
2021-05-28 01:11:27 +0530

    using if and else

--- new/new.js
+++ new/new.js
@@ -2,11 +2,6 @@
 new Vue({
     el: '#app',
     data: {
-        styleObj:{
-          backgroundColor: "blue",
-          fontSize: '30px'
-        },
-        size: '3em',
-        bgcolor: 'coral'
+        darkTitle: true
     },
 })
