9fd2c26c0f9cceb755ca0acc7af562744ad66efc
FarisPalayi <fariswritecode@gmail.com>
2021-05-27 20:27:54 +0530

    like button

--- instance/instance.js
+++ instance/instance.js
@@ -1,6 +1,8 @@
-var data = { newData: 'click me', buttonHtml: '<button>hello my dear friends!</button>' };
+
 
 let vm = new Vue({
     el: '#app',
-    data: data,
+    data: {
+        likes: 0
+    }
 })
