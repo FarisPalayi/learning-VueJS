3b2b50ef8c97ac4a56acf048ba8f442d2f02e7a3
FarisPalayi <fariswritecode@gmail.com>
2021-05-27 20:01:57 +0530

    showing raw html from data

--- instance/instance.js
+++ instance/instance.js
@@ -1,17 +1,6 @@
-var data = { newData : "hello", newNewData: null };
+var data = { newData: 'click me', buttonHtml: '<button>hello my dear friends!</button>' };
 
 let vm = new Vue({
     el: '#app',
     data: data,
-    created: function () { // this is a hook which runs after an instance is created.
-        console.log('newNewData is: ' + this.newNewData);
-    }
-})
-
-vm.newData = "this is the new overrided blah text";
-vm.newNewData = 'hello my dear friends!';
-
-vm.$watch('newNewData', function(newValue, oldValue){ // this function is called when the value of the newNewData changes
-    console.log(newValue);
-    console.log(oldValue);
 })
