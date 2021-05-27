b7114df157b89def888cea7f88acd305f716d726
FarisPalayi <fariswritecode@gmail.com>
2021-05-27 23:12:26 +0530

    computed properties

--- new/new.js
+++ new/new.js
@@ -1,13 +1,19 @@
-new Vue({
+let vm = new Vue({
   el: "#app",
   data: {
     submit: "Submit",
-    style: null
+    style: null,
+    message: "How are things going?",
   },
   methods: {
     submitData: function () {
       this.submit = "Sumitted";
-      this.style = 'background-color: lime';
+      this.style = "background-color: lime";
+    },
+  },
+  computed: {
+    reversedMessage: function () {
+      return this.message.split("").reverse().join("");
     },
   },
 });
