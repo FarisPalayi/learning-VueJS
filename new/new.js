5279c6d760c1c5b5743f8ad9f311fde79a7d16ed
FarisPalayi <fariswritecode@gmail.com>
2021-05-28 00:05:39 +0530

    watcher

--- new/new.js
+++ new/new.js
@@ -1,25 +1,42 @@
-let vm = new Vue({
-  el: "#app",
+var watchExampleVM = new Vue({
+  el: "#watch-example",
   data: {
-    submit: "Submit",
-    firstName: "Peter",
-    lastName: "Parker",
-    fullName: "Peter Parker"
+    question: "",
+    answer: "I cannot give you an answer until you ask a question!",
+  },
+  watch: {
+    // whenever question changes, this function will run
+    question: function (newQuestion, oldQuestion) {
+      this.answer = "Waiting for you to stop typing...";
+      this.debouncedGetAnswer();
+    },
+  },
+  created: function () {
+    // _.debounce is a function provided by lodash to limit how
+    // often a particularly expensive operation can be run.
+    // In this case, we want to limit how often we access
+    // yesno.wtf/api, waiting until the user has completely
+    // finished typing before making the ajax request. To learn
+    // more about the _.debounce function (and its cousin
+    // _.throttle), visit: https://lodash.com/docs#debounce
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 500);
   },
   methods: {
-    submitData: function () {
-      this.submit = "Sumitted";
-      this.style = "background-color: lime";
+    getAnswer: function () {
+      if (this.question.indexOf("?") === -1) {
+        this.answer = "Questions usually contain a question mark. ;-)";
+        return;
+      }
+      this.answer = "Thinking...";
+      var vm = this;
+      axios
+        .get("https://yesno.wtf/api")
+        .then(function (response) {
+          vm.answer = _.capitalize(response.data.answer);
+        })
+        .catch(function (error) {
+          vm.answer = "Error! Could not reach the API. " + error;
+        });
     },
-  },
-  computed: {
-    get: function(){
-        return this.firstName + " " + this.lastName;
-    },
-    set: function(value){
-        let names = value.split(' ');
-        let firstName = names[0];
-        let lastName = names[names.length - 1];
-    }
   },
 });
