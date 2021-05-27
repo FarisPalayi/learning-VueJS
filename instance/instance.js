1407b7233cfffaba196b5cbbff2d11e2f7448d75
FarisPalayi <fariswritecode@gmail.com>
2021-05-27 19:35:22 +0530

    `created` hook => runs after an instance is created

--- instance/instance.js
+++ instance/instance.js
@@ -3,12 +3,15 @@ var data = { newData : "hello", newNewData: null };
 let vm = new Vue({
     el: '#app',
     data: data,
+    created: function () { // this is a hook which runs after an instance is created.
+        console.log('newNewData is: ' + this.newNewData);
+    }
 })
 
 console.log(vm.$data == data);
 
 vm.newData = "this is the new overrided blah text";
-vm.newNewData = 'hello idiots!';
+vm.newNewData = 'hello my dear friends!';
 
 vm.$watch('newNewData', function(newValue, oldValue){ // this function is called when the value of the newNewData changes
     console.log(newValue);
