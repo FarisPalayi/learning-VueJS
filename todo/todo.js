7effa01dd59261cb5c5bfac84831375a2c45c937
FarisPalayi <fariswritecode@gmail.com>
2021-05-30 00:54:58 +0530

    component -> data as a function

---
+++ todo/todo.js
@@ -0,0 +1,27 @@
+let vm = new Vue({
+  el: "#app",
+  data: {
+    placeholder: "Add a todo ⌨",
+    items: [
+      { id: 0, text: "first todo item", isChecked: false },
+      { id: 1, text: "second todo item", isChecked: false },
+      { id: 2, text: "third todo item", isChecked: false },
+    ],
+    nextTodoId: 4,
+    newTodoText: "",
+  },
+  methods: {
+    addItem: function () {
+      if (this.newTodoText === "") return;
+      this.items.push({ id: this.nextTodoId++, text: this.newTodoText });
+      this.newTodoText = "";
+    },
+  },
+});
+
+Vue.component('hyperlink', {
+  template: '<a href="../index.html" class="hyperlink">home</a>'
+})
+let footer = new Vue({
+  el: '#footer',
+})
