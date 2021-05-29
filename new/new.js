233e2da800f6289baa17f1b6355fdd8011ce2572
FarisPalayi <fariswritecode@gmail.com>
2021-05-30 02:23:41 +0530

    $emit

--- new/new.js
+++ new/new.js
@@ -1,8 +1,23 @@
-Vue.component('button-counter', {
-  data: function() {
-    return { count: 0 }
-  },
-  template: '<button @click="count++"> You clicked me {{ count }} times.</button>'
+Vue.component('blog-post', {
+  props: ['post'],
+  template: `
+    <div class="blog-post">
+      <h3>{{ post.title }}</h3>
+      <button v-on:click="$emit('enlarge-text')">
+        Enlarge text
+      </button>
+      <div v-html="post.content"></div>
+    </div>
+  `
 })
-
-new Vue({ el: '#app'})
+new Vue({
+  el: '#app',
+  data: {
+    posts: [
+      { id: 1, title: 'My journey with Vue' },
+      { id: 2, title: 'Blogging with Vue' },
+      { id: 3, title: 'Why Vue is so fun' }
+    ],
+    postFontSize: 1
+  },
+})
