a941288fa1a7d5f924d9852707df143a9405a003
FarisPalayi <fariswritecode@gmail.com>
2021-07-08 17:15:59 +0530

    👼 first vuex app - counter app

--- new/new.js
+++ new/new.js
@@ -1,8 +1,35 @@
-Vue.component('blog-post', {
-  template: `<div`
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    },
+    decrement (state) {
+      state.count--
+    }
+  }
 })
+
+store.commit('increment')
+
+console.log(store.state.count)
 
 new Vue({
   el: '#app',
-  data: {  },
+  store,
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+    },
+    decrement() {
+      this.$store.commit('decrement')
+    }
+  }
 })
