750b8eff76edab9e0715cfb6a746f9916a1c02d4
FarisPalayi <farispalayi@gmail.com>
2021-10-14 07:56:20 +0530

    minor changes

--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,17 @@
-import Home from "../Pages/Home/Home.js"
-import Cards from "../Pages/Cards/Cards.js"
-import Todo from "../Pages/Todo/Todo.js";
-import Likes from "../Pages/Likes/Likes.js";
-import Blog from "../Pages/Blog/Blog.js";
+import Home from "../pages/Home/Home.js";
+import Cards from "../pages/Cards/Cards.js";
+import Todo from "../pages/Todo/Todo.js";
+import Likes from "../pages/Likes/Likes.js";
+import Blog from "../pages/Blog/Blog.js";
 import FullArticle from "../components/Blog/FullArticle/FullArticle.js";
 import WriteBlog from "../components/Blog/WriteBlog/WriteBlog.js";
 
 export default [
-  { path: "/",        component: Home,       name: "Home"  },
-  { path: "/cards",   component: Cards,      name: "Cards" },
-  { path: "/todo",    component: Todo,       name: "Todo"  },
-  { path: "/blog",    component: Blog,       name: "Blog"  },
-  { path: "/article", component: FullArticle               }, // if no name specified, it'll not be included in the navbar links
-  { path: "/likes",   component: Likes,      name: "Likes" },
-  { path: "/write",   component: WriteBlog,                },
+  { path: "/", component: Home, name: "Home" },
+  { path: "/cards", component: Cards, name: "Cards" },
+  { path: "/todo", component: Todo, name: "Todo" },
+  { path: "/blog", component: Blog, name: "Blog" },
+  { path: "/article", component: FullArticle }, // if no name specified, it'll not be included in the navbar links
+  { path: "/likes", component: Likes, name: "Likes" },
+  { path: "/write", component: WriteBlog },
 ];
