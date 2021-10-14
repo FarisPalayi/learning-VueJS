a76cf13081111abce8e171912358f2619da0aa51
FarisPalayi <farispalayi@gmail.com>
2021-10-14 07:23:47 +0530

    fix: add defer to script tags

--- routes/routes.js
+++ routes/routes.js
@@ -6,10 +6,10 @@ import Blog from "../pages/Blog/Blog.js";
 import FullArticle from "../components/Blog/FullArticle/FullArticle.js";
 
 export default [
-  { path: "/", component: Home, name: "Home" },
-  { path: "/cards", component: Cards, name: "Cards" },
-  { path: "/todo", component: Todo, name: "Todo" },
-  { path: "/blog", component: Blog, name: "Blog" },
-  { path: "/article", component: FullArticle }, // if no name specified, it'll not be included in the navbar links
-  { path: "/likes", component: Likes, name: "Likes" },
+  { path: "/",        component: Home,       name: "Home"  },
+  { path: "/cards",   component: Cards,      name: "Cards" },
+  { path: "/todo",    component: Todo,       name: "Todo"  },
+  { path: "/blog",    component: Blog,       name: "Blog"  },
+  { path: "/article", component: FullArticle               }, // if no name specified, it'll not be included in the navbar links
+  { path: "/likes",   component: Likes,      name: "Likes" },
 ];
