e5e64f51124e695f144e9f36d3ec32c3bf46409c
FarisPalayi <farispalayi@gmail.com>
2021-10-14 06:22:25 +0530

    add store folder to archive

---
+++ pages/Search/Search.js
@@ -0,0 +1,14 @@
+const { data, methods } = {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+
+const Search = {
+  data,
+  methods,
+  template: `<div>html goes here</div>`,
+};
+
+export default Vue.component("search", Search);
