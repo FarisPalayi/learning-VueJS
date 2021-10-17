51383fed7d3f4d61507e24bf6a342f105c9a06c5
FarisPalayi <farispalayi@gmail.com>
2021-10-17 07:07:24 +0530

    remove likes page

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,6 @@
 import Home from "../pages/Home/Home.js";
 import Cards from "../pages/Cards/Cards.js";
 import Todo from "../pages/Todo/Todo.js";
-import Likes from "../pages/Likes/Likes.js";
 import Blog from "../pages/Blog/Blog.js";
 import FullArticle from "../components/Blog/FullArticle/FullArticle.js";
 import WriteBlog from "../components/Blog/WriteBlog/WriteBlog.js";
@@ -13,7 +12,6 @@ export default [
   { path: "/todo", component: Todo, name: "Todo" },
   { path: "/blog", component: Blog, name: "Blog" },
   { path: "/article/:id", component: FullArticle }, // if no name specified, it'll not be included in the navbar links
-  { path: "/likes", component: Likes, name: "Likes" },
   { path: "/write", component: WriteBlog },
   { path: "/search", component: Search, name: "Words" },
 ];
